6aeeb61fea2ff2b03a0c41d908dfa8fd6141e4a3
PARK, Yeonjong <yeonjong.park@outlook.com>
2025-07-17 17:51:25 +0900

    스웨거 설정 추가

--- src/main/java/kr/ac/kumoh/likelion/bouquet/global/config/swagger/SwaggerConfig.java
+++ src/main/java/kr/ac/kumoh/likelion/bouquet/global/config/swagger/SwaggerConfig.java
@@ -1,8 +1,11 @@
 package kr.ac.kumoh.likelion.bouquet.global.config.swagger;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.beans.factory.annotation.Value;
@@ -22,11 +25,15 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("Access Token");
+
         return new OpenAPI()
                 .info(apiInfo())
                 .addServersItem(new Server()
                         .url(serverUrl)
                         .description(serverDescription))
+                .addSecurityItem(securityRequirement)
+                .components(securitySchemes())
                 ;
     }
 
@@ -51,5 +58,17 @@ public class SwaggerConfig {
                         "<p>입력 유효성 오류는 명시돼 있지 않으며 <code>4XX</code> 상태 코드를 반환합니다." +
                         "<code>5XX</code> 상태 코드의 경우 정의되지 않은 서버 오류이므로 담당자에게 문의해 주세요.</p>")
                 .version("1.0.0");
+    }
+
+    private Components securitySchemes() {
+        final SecurityScheme accessTokenSecurityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("Bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        return new Components()
+                .addSecuritySchemes("Access Token", accessTokenSecurityScheme);
     }
 }
