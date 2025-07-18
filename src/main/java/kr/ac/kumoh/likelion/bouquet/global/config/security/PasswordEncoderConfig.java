b1c52704e8a511bc9607f1bb9cda4413401f3607
PARK, Yeonjong <yeonjong.park@outlook.com>
2025-07-18 22:40:19 +0900

    비밀번호 암호화를 위한 BCrypt 인코더 빈 추가

---
+++ src/main/java/kr/ac/kumoh/likelion/bouquet/global/config/security/PasswordEncoderConfig.java
@@ -0,0 +1,14 @@
+package kr.ac.kumoh.likelion.bouquet.global.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordEncoderConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
